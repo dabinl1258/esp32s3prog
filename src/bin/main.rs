19f49d1d97af1df4e28bd9c8ba69e406864307ca
dabinl1258 <ldbl1258@gmail.com>
2026-07-22 22:08:03 +0900

    make struct

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -23,6 +23,131 @@ use esp_hal::main;
 use esp_hal::time::{Duration, Instant};
 use esp_println as _;
 
+struct S3interface<'a> {
+    reset: Output<'a>,
+    vpp: Output<'a>,
+    sclk: Output<'a>,
+    sdat: Flex<'a>,
+
+    time_setup_start: u32,
+    time_setup_hold: u32,
+    time_clk_low: u32,
+    time_clk_high: u32,
+    delay: Delay,
+}
+
+impl<'a> S3interface<'a> {
+    pub fn new(reset: Output<'a>, vpp: Output<'a>, sclk: Output<'a>, sdat: Flex<'a>) -> Self {
+        Self {
+            reset,
+            vpp,
+            sclk,
+            sdat,
+            time_setup_start: 100,
+            time_setup_hold: 100,
+            time_clk_low: 100,
+            time_clk_high: 100,
+            delay: Delay::new(),
+        }
+    }
+
+    pub fn init(&mut self) {
+        self.reset.set_high();
+        self.vpp.set_low();
+        self.sclk.set_low();
+
+        self.sdat.set_input_enable(false);
+        self.sdat.set_output_enable(true);
+        self.sdat.set_low();
+    }
+
+    /// 프로그래밍 모드 시작 함수
+    pub fn enterProgramMode(&mut self) {
+        self.reset.set_low();
+        self.vpp.set_high();
+    }
+
+    pub fn dummyClk(&mut self) {
+        self.sdat.set_input_enable(false);
+        self.sdat.set_output_enable(true);
+
+        self.delay.delay_micros(self.time_clk_low);
+        self.sdat.set_high();
+        self.sclk.set_high();
+        self.delay.delay_micros(self.time_clk_high);
+        self.sclk.set_low();
+        self.sdat.set_low();
+
+        self.sdat.set_input_enable(true);
+        self.sdat.set_output_enable(false);
+    }
+
+    pub fn read(&mut self, addr: u16, len: usize) -> [u8; 128] {
+        let byte1: u8 = 0x61u8;
+        for i in (0..=7).rev() {
+            //info!("i : {}", i);
+            if ((0x01 << i) & byte1) != 0 {
+                self.sdat.set_high();
+            } else {
+                self.sdat.set_low();
+            }
+
+            self.delay.delay_micros(self.time_clk_low);
+            self.sclk.set_high();
+            self.delay.delay_micros(self.time_clk_high);
+            self.sclk.set_low();
+        }
+        self.dummyClk();
+        for i in (0..=15).rev() {
+            if ((0x01u16 << i) & addr) != 0 {
+                self.sdat.set_high();
+            } else {
+                self.sdat.set_low();
+            }
+
+            self.delay.delay_micros(self.time_clk_low);
+            self.sclk.set_high();
+            self.delay.delay_micros(self.time_clk_high);
+            self.sclk.set_low();
+            if i == 8 || i == 0 {
+                self.dummyClk();
+            }
+        }
+
+        let mut mem: [u8; 128] = [0; 128];
+        for i in 0..len {
+            mem[i] = self.readbyte();
+        }
+        mem
+    }
+    pub fn readbyte(&mut self) -> u8 {
+        let mut readed: u8 = 0u8;
+        for i in (0..=7).rev() {
+            self.delay.delay_micros(self.time_clk_low);
+            self.sclk.set_high();
+            self.delay.delay_micros(self.time_clk_high / 2);
+            if self.sdat.is_high() {
+                readed = readed | (0x01 << i);
+            }
+            self.delay.delay_micros(self.time_clk_high / 2);
+            self.sclk.set_low();
+        }
+        readed
+    }
+    pub fn startCondition(&mut self) {
+        self.sdat.set_input_enable(false);
+        self.sdat.set_output_enable(true);
+        self.sclk.set_high();
+        self.delay.delay_nanos(self.time_setup_start);
+        self.sdat.set_high();
+        self.delay.delay_nanos(self.time_setup_hold);
+    }
+    pub fn stopCondition(&mut self) {
+        self.sdat.set_input_enable(false);
+        self.sdat.set_output_enable(true);
+    }
+}
+
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
@@ -158,8 +283,14 @@ fn main() -> ! {
         loop {
             let delay_start = Instant::now();
             while delay_start.elapsed() < Duration::from_millis(500) {}
+            break;
         }
+        break;
     }
+    let mut s3 = S3interface::new(reset, vpp, sclk, sdat);
+    s3.init();
+    s3.enterProgramMode();
+    loop {}
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.1.0/examples
 }
